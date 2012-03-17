226fc7a921a584d9e2262de12e2f847967868b78
Daniel Brain <github@biboop.com>
2012-03-17 20:19:18 +1100

    Add locality validation

--- modules/vehiclecontact-app.js
+++ modules/vehiclecontact-app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = module.exports = express.createServer();
 var api = require('./api');
+var localityImporter = require('../lib/localityImporter');
 
 function handleError(err, req, res) {
   res.send({ error: 'server_error', gibberish: { message: err.message, stack: err.stack } }, 500);
@@ -8,5 +9,6 @@ function handleError(err, req, res) {
 
 app.configure(function () {
   app.error(handleError);
+  localityImporter.importIfMissing();
   app.use('/api', api);
 });
